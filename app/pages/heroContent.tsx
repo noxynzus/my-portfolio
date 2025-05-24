c336a54c080e081a421ad09dd0f582d10f00b7ef
dev-arrthawat <dev.arrthawat@gmail.com>
2025-05-24 11:29:28 +0700

    add/change-to-vc
    - Change resume to cv file
    - clean code
    - fixed mobile navigation

--- app/pages/heroContent.tsx
+++ app/pages/heroContent.tsx
@@ -59,9 +59,9 @@ export default function HeroContent() {
         </div>
 
         <LinkButtonOutline
-          href={"/images/resume/last-resume.png"}
-          text="Download Resume"
-          download="last-resume.png"
+          href={"/images/resume/cv-profile.pdf"}
+          text="Download CV"
+          download="cv-profile.pdf"
           icon={
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -81,52 +81,11 @@ export default function HeroContent() {
         />
       </motion.div>
 
-      {/* Right */}
-      {/* <motion.div
-        ref={ref}
-        initial={{ opacity: 0 }}
-        animate={isInView ? { opacity: 1 } : {}}
-        transition={{ duration: 1 }}
-        className="hidden lg:flex xl:flex justify-center items-center w-full "
-      >
-        <Image
-          src="/images/backgrounds/bg-code.png"
-          alt="Hero Image"
-          width={500}
-          height={500}
-          className="absolute -z-10"
-        />
-      </motion.div> */}
-
       <div className="mask-radial-at-bottom mask-radial-from-100% ">
         <div className="z-2 absolute left-1/4 top-20 -translate-x-1/2 transform rounded-full border-[500px] border-b-blue-400 border-l-violet-600 border-r-pink-500 border-t-purple-400 blur-[240px]"></div>
         <div className="z-2 absolute right-[25vw] top-[50vh] rounded-full border-[300px] border-b-cyan-400 border-l-rose-600 border-r-indigo-500 border-t-blue-400 blur-[200px]"></div>
       </div>
 
-      {/* <div
-        className="absolute z-10 inset-x-0 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      >
-        <div className="relative rounded-full left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] translate-x-1/2 bg-linear-to-tr from-[#0070f3]  to-[#00a6f4] opacity-15 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
-      </div> */}
-
-
-
-    {/* <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-      <svg viewBox="0 0 1024 1024" className="absolute top-1/2 left-1/2 -z-10 size-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
-        <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
-        <defs>
-          <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-            <stop stop-color="#7775D6" />
-            <stop offset="1" stop-color="#E935C1" />
-          </radialGradient>
-        </defs>
-      </svg>
-      <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-       
-      </div>
-   
-    </div> */}
     </section>
   );
 }
